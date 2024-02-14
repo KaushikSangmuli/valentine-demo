b6c851d845ad1198d091c1b8a43cc92428d3ad6d
kaushik sangmuli <sangmulekaoushik@gmail.com>
2024-02-14 22:53:49 +0530

    proportion and buttons fixed

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 let body= document.querySelector("body")
 let inputs = document.querySelector("#enterName");
 let greet = document.querySelector(".greet");
-
 let submit = document.querySelector("#submit")
+let choices = document.querySelector(".choices")
 submit.addEventListener ( "click" , ()=>{
     let hername = inputs.value;
      greet.innerText= " hello " + hername + ", I am glad you came here"
      greet.classList.remove("hidden")
-
+     choices.classList.remove("hidden")
      console.log(hername)
 }
 )
+let chocolate = document.querySelector("#chocolate");
+let flower = document.querySelector("#flower");
+let chocoImg = document.querySelector ("#mychoc");
+let  flowImg =document.querySelector ("#myflow")
 
+let outingQ = document.querySelector ("#outingQ")
+let wantque = document.querySelector("#want")
+
+chocolate.addEventListener ( "click", ()=>{
+    chocoImg.classList.remove("hidden")
+    flower.classList.add("hidden")
+    chocolate.classList.add("hidden")
+    outingQ.classList.remove("hidden")
+    wantque.classList.remove("hidden")
+})
+flower.addEventListener ( "click" , () =>{
+    flowImg.classList.remove ("hidden")
+    flower.classList.add("hidden")
+    chocolate.classList.add("hidden")
+    outingQ.classList.remove("hidden")
+    wantque.classList.remove("hidden")
+} )
+let outing = document.querySelector("#outing")
+wantque.addEventListener ("click", () =>{
+    outing.classList.remove("hidden")
+
+
+})
 
